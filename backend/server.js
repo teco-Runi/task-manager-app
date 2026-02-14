6ef721ec38f535b1cfc68ebe9c6cfa54b3c6670d
teco-Runi <rutuja1308gaikwad@gmail.com>
2026-02-14 13:29:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const routes = require('./routes/...');
 require("dotenv").config();
 
 const app = express();
