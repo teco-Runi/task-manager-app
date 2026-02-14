b4568f5b2af4381f964d13db50eac540dfceda65
teco-Runi <rutuja1308gaikwad@gmail.com>
2026-02-14 12:05:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 // ===== MIDDLEWARE =====
 app.use(cors());
 app.use(express.json());
